6e00581ce3f6939bc64a3d64c5dccda855f240d6
aland1013 <adam.land@gmail.com>
2020-02-13 20:21:23 -0500

    solved second stretch challenge

--- index.js
+++ index.js
@@ -196,8 +196,10 @@ function getFullNames(runners) {
  * @returns an array with all the runners' first names in ALL CAPS.
  * The first names appear in the array in the same order the runners appear in the `runners` array.
 */
-function firstNamesAllCaps(/* CODE HERE */) {
-  /* CODE HERE */
+function firstNamesAllCaps(runners) {
+  let allCaps = [];
+  runners.forEach(runner => allCaps.push(runner.first_name.toUpperCase()));
+  return allCaps;
 }
 
 /**
